f3c71267fb61904aa4b68673cc0631a08c7faaba
Ralph Chaanine <10029584+ralphchaanine@users.noreply.github.com>
2019-05-24 14:42:56 +0300

    fixed the sun/moon's X position being incorrectly tracked in the shader

--- assets/shaders/cycle_fragment.glsl
+++ assets/shaders/cycle_fragment.glsl
@@ -18,7 +18,6 @@ void main() {
    	vec3 V = (out_vertex);
     vec3 L = (planetPosition);
 
-    //////////////////////////////////////////////////////////////
     //Since un-normalized coordinates in openGL start at the
     //top left, a lower y translates to a higher position.
     //
@@ -28,10 +27,12 @@ void main() {
     //
     //Keep in mind that it never actually gets to the very top, more
     //like 80% or 90%. Maybe fix this later [DayNightCycle.cpp]
-    //
-    ////////////////////////////////////////////////////////////////
-
 	L.y = 1 - L.y;
+
+	//Additionally, since 0 is the center in OpenGL, we're going to
+	//convert our planet's X position from the 0, 1 range (where 0.5
+	//is the center to the -1, 1 range (where 0 is the center)
+	L.x = (L.x * 2) - 1;
 
 	//Read from the lefthand side of the sky file if it's nighttime
 	if(!daytime) {
@@ -47,10 +48,10 @@ void main() {
 	//its right half dedicated to the daytime gradient and
 	//left half to the nighttime gradient
 	vec4 Kc = texture2D(sky, 	vec2((L.y + 1) / 2, V.y));
-    //vec4 Kg = texture2D(glow,  	vec2((L.y + 1) / 2, vl));
+    vec4 Kg = texture2D(glow,  	vec2((L.y + 1) / 2, vl));
 	//fragColor = vec4(vl,vl,vl,255);
-	fragColor = Kc;
+	//fragColor = Kc;
 
     //Combine the sky and glow giving the pixel value.
-    //fragColor = vec4(Kc.rgb + Kg.rgb * Kg.a / 2.0, Kc.a);
+    fragColor = vec4(Kc.rgb + Kg.rgb * Kg.a / 2.0, Kc.a);
 }
